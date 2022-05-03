d658e9172c28edadc7d6b838d782d274f04e2af4
= <chsan626@gmail.com>
2022-05-04 01:03:30 +0900

    대화창 구현 중

--- Test/Assets/Scripts/Dialog/Dialog.cs
+++ Test/Assets/Scripts/Dialog/Dialog.cs
@@ -3,171 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[System.Serializable]
 public class dialog_info
 {
-    public string name;
-    [TextArea(3, 5)]
-    public string content;
-    public bool check_read;
+    public string guestName;
+    Dictionary<int, string[]> Data;
 }
 
-[System.Serializable]
-public class Dialog_cycle
+public class Dialog : MonoBehaviour
 {
-    public string cycle_name;
-    public List<dialog_info> info = new List<dialog_info>();
-    public int cycle_index;
-    public bool check_cycle_read;
-}
+    string test_char = "짱구"; // 테스트를 위한 임시 이름 ( 손님의 이름을 가져왔다고 가정)
+    string test_dialog = "천방지축 얼렁뚱땅 빙글빙글 돌아가는 짱구네 하루";
 
-public class parameter : MonoBehaviour
-{
-    public Text name_text;
-    public Text content;
-    public Text next_text;
-}
+    public GameObject textPanel; // 대화 창
+    public Text GuestText; // 대화가 진행 될 텍스트
+    private Dictionary<int, string[]> dialogData;
+    public bool isReading;
+    private int DialogIndex;
 
-public class dialog : MonoBehaviour
-{
-    [SerializeField]
-    public static dialog instance = null;
-    public List<Dialog_cycle> dialog_cycles = new List<Dialog_cycle>(); //대화 지문 그룹
-    public Queue<string> text_seq = new Queue<string>();                //대화 지문들의 내용을 큐로 저장한다.(끝점을 쉽게 판단하기 위해)
-    public string name_;                                                //임시로 저장할 대화 지문의 이름
-    public string text_;                                                //임시로 저장할 대화 지문의 내용
-
-    public Text nameing;                                                //대화 지문 오브젝트에 있는 것을 표시할 오브젝트
-    public Text DialogT;                                                //대화 지문 내용 오브젝트
-    public Text Next_T;                                                 //다음 버튼
-    public GameObject dialog_obj;                                       //대화 지문 오브젝트
-
-    IEnumerator seq_;
-    IEnumerator skip_seq;
-
-    public float delay;
-    public bool running = false;
-
-
-    void Awake()    //싱글톤 패턴으로 어느 씬에서든 접근 가능하게 한다.
+    private void Awake()
     {
-        if (instance == null)
-            instance = this;
-
-        else if (instance != this)
-            Destroy(gameObject);
-
-        DontDestroyOnLoad(gameObject);
+        DialogIndex = 0;
+        dialogData = new Dictionary<int,string[]>();
+        loadDialog();
     }
 
-    public void skip(int index)
+    // 손님의 이름을 읽어온다.
+    private void loadGuest()
     {
-        StopAllCoroutines();
-        dialog_obj.SetActive(false);
-        dialog_cycles[index].check_cycle_read = true;
-        running = false;
-    }
-    public IEnumerator dialog_system_start(int index)//다이얼로그 출력 시작
-    {
-        nameing = dialog_obj.GetComponent<parameter>().name_text;   //다이얼로그 오브젝트에서 각 변수 받아오기
-        DialogT = dialog_obj.GetComponent<parameter>().content;
-        Next_T = dialog_obj.GetComponent<parameter>().next_text;
-
-        running = true;
-        foreach (dialog_info dialog_temp in dialog_cycles[index].info)  //대화 단위를 큐로 관리하기 위해 넣는다.
-        {
-            text_seq.Enqueue(dialog_temp.content);
-        }
-
-        dialog_obj.gameObject.SetActive(true);
-        for (int i = 0; i < dialog_cycles[index].info.Count; i++)       //대화 단위를 순서대로 출력
-        {
-
-            nameing.text = dialog_cycles[index].info[i].name;
-
-            text_ = text_seq.Dequeue();                                  //대화 지문을 pop
-
-            seq_ = seq_sentence(index, i);                               //대화 지문 출력 코루틴
-            StartCoroutine(seq_);                                        //코루틴 실행
-
-
-            yield return new WaitUntil(() =>
-            {
-                if (dialog_cycles[index].info[i].check_read)            //현재 대화를 읽었는지 아닌지
-                {
-                    return true;                                        //읽었다면 진행
-                }
-                else
-                {
-                    return false;                                       //읽지 않았다면 다시 검사
-                }
-            });
-        }
-
-        dialog_cycles[index].check_cycle_read = true;                   //해당 대화 그룹 읽음
-        running = false;
+          
     }
 
-    public void DisplayNext(int index, int number)                      //다음 지문으로 넘어가기
+    // 손님의 이름을 이용하여 그에 해당하는 텍스트를 파일에서 불러온다.
+    private void loadDialog()
     {
-        Next_T.text = "";
-        Next_T.gameObject.SetActive(false);
-
-        if (text_seq.Count == 0)                                        //다음 지문이 없다면
-        {
-            dialog_obj.gameObject.SetActive(false);                     //다이얼로그 끄기
-        }
-        StopCoroutine(seq_);                                            //실행중인 코루틴 종료
-
-        dialog_cycles[index].info[number].check_read = true;            //현재 지문 읽음으로 표시
+        //만족도에 따라 다른 대화창을 가져온다.
+        dialogData.Add(1, new string[] { "안녕", "나는 짱구야", "만나서 반가워" }); // 불러왔다고 가정하고 테스트 문장 삽입
+    }
+    
+    public string GetDialog(int sat, int dialogindex)
+    {
+        return dialogData[sat][dialogindex];
     }
 
-    public IEnumerator seq_sentence(int index, int number)              //지문 텍스트 한글자식 연속 출력
+    IEnumerator WaitForIt(float time)
     {
-        skip_seq = touch_wait(seq_, index, number);                     //터치 스킵을 위한 터치 대기 코루틴 할당
-        StartCoroutine(skip_seq);                                       //터치 대기 코루틴 시작
-        DialogT.text = "";                                              //대화 지문 초기화
-        foreach (char letter in text_.ToCharArray())                    //대화 지문 한글자씩 뽑아내기
-        {
-            DialogT.text += letter;                                     //한글자씩 출력
-            yield return new WaitForSeconds(delay);                     //출력 딜레이
-        }
-        Next_T.gameObject.SetActive(true);
-        Next_T.text = "next";
-        StopCoroutine(skip_seq);                                        //지문 출력이 끝나면 터치 대기 코루틴 해제
-        IEnumerator next = next_touch(index, number);                   //버튼 이외에 부분을 터치해도 넘어가는 코루틴 시작
-        StartCoroutine(next);
+        yield return new WaitForSeconds(time);
+    }
+    
+    // 대화창에서 대화 내용을 출력한다.
+
+    public void readDialog()
+    {
+        textPanel.SetActive(true);
+        // 기본적으로 빈 텍스트에서 대화 내용을 한 글자씩 추가하여 출력하고 딜레이 하기를 반복한다.
+        GuestText.text = GetDialog(1, DialogIndex);
+        DialogIndex++;
+        Debug.Log(GuestText.text);
+        StartCoroutine(WaitForIt(1000.0f));
+
+
+        // 텍스트 창을 클릭한 경우, 문장이 한번에 출력이 된다.
+
+
+        // 텍스트가 모두 출력이 된 경우에 클릭 시, 다음 문장이 출력된다.
+
+
+        // 어느 문장까지 출력하였는지 저장한다.
+
+
+        // 대화 내용을 모두 출력하고 나면 손님 응대에 관한 여부를 플레이어에게 묻는다. (받는다/ 받지 않는다)
+        takeGuest();
     }
 
-    public IEnumerator touch_wait(IEnumerator seq, int index, int number)//터치 대기 코루틴
+    private void takeGuest()
     {
-        yield return new WaitForSeconds(0.3f);
-        yield return new WaitUntil(() => Input.GetMouseButton(0));
-        StopCoroutine(seq);                                              //대화 지문 코루틴 해제
-        DialogT.text = text_;                                            //스킵시 모든 지문 한번에 출력
-        Next_T.gameObject.SetActive(true);
-        Next_T.text = "next";
-        IEnumerator next = next_touch(index, number);                    //대화 지문 코루틴 해제 됬기 때문에 다음 지문으로 가는 코루틴 시작
-        StartCoroutine(next);
-    }
 
-    public IEnumerator next_touch(int index, int number)    //터치로 다음 지문 넘어가는 코루틴
-    {
-        StopCoroutine(seq_);
-        StopCoroutine(skip_seq);
-        yield return new WaitForSeconds(0.3f);
-        yield return new WaitUntil(() => Input.GetMouseButton(0));
-        DisplayNext(index, number);
     }
-
-    public bool dialog_read(int check_index)          //index의 부분을 읽었는지 확인하는 함수
-    {
-        if (!dialog_cycles[check_index].check_cycle_read)
-        {
-            return true;
-        }
-
-        return false;
-    }
+    
 
 }
